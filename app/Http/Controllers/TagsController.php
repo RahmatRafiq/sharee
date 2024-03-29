b9be43480a7dd4b7fe26d7f47eb2c4358765b61c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-03-30 02:31:42 +0800

    Add validation and update functionality to TagsController

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -5,75 +5,50 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Tags;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class TagsController extends Controller
 {
- 
+
     public function index()
     {
         $tags = Tags::all();
         return view('backend.tags.index', compact('tags'));
     }
 
-   
-    public function create()
-    {
-        
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'nama_tags' => 'required|min:2',
+        ]);
+
+        $tags = Tags::create([
+            'nama_tags' => $request->nama_tags,
+            'slug' => Str::slug($request->nama_tags),
+        ]);
+
+        return redirect()->route('backend.tags.index')->with(['success' => 'Data Berhasil Disimpan']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'nama_tags' => 'required|min:2',
+        ]);
+
+        $tags = Tags::findOrFail($id);
+        $tags->nama_tags = $request->nama_tags;
+        $tags->slug = Str::slug($request->nama_tags);
+        $tags->save();
+
+        return redirect()->route('backend.tags.index')->with(['success' => 'Data Berhasil Diperbarui']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $tags = Tags::findOrFail($id);
+        $tags->delete();
+
+        return redirect()->route('backend.tags.index')->with(['success' => 'Data Berhasil Dihapus']);
     }
 }
