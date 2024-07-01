44e03fadb4964383a7e92760e73315b7dc0a3d6c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-07-01 15:11:19 +0800

    add select2 on blade artikel create and edit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -29,7 +29,10 @@
 	<link rel="stylesheet" href="{{ asset('backend/css/atlantis.min.css') }}">
 
 	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="{{ asset ('backend/css/demo.css')}}">
+	<link rel="stylesheet" href="{{ asset('backend/css/demo.css') }}">
+
+	<!-- Select2 CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -49,8 +52,25 @@
 			@include('includes.footer')
 		</div>
 	</div>
+
 	<!--   Core JS Files   -->
 	@include('includes.js')
+
+	<!-- jQuery -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+	<!-- Select2 JS -->
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+	<!-- Inisialisasi Select2 -->
+	<script>
+		$(document).ready(function() {
+			$('.select2').select2({
+				placeholder: "Pilih tags",
+				allowClear: true
+			});
+		});
+	</script>
 </body>
 
 </html>
